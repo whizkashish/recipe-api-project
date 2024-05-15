6851dcc08fcbba197def4168061c797dd82122f9
kashish <immi.kashish@gmail.com>
2024-05-15 15:42:30 +0530

    Fixed Linting

--- app/core/models.py
+++ app/core/models.py
@@ -8,16 +8,18 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+
 class UserManager(BaseUserManager):
     "Manager for users"
 
     def create_user(self, email, password=None, **extra_fields):
         """ Create, save and return a new user """
-        user = self.model(email=email,**extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
-        
+
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
--- app/core/test_models.py
+++ app/core/test_models.py
@@ -4,6 +4,7 @@ Tests for models
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     """Test models."""
 
