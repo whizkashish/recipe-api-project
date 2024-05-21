1fb43464ac6aa1f192de215236eaa59ec81c5820
kashish <immi.kashish@gmail.com>
2024-05-21 09:34:07 +0530

    Deploy Process

---
+++ app/core/views.py
@@ -0,0 +1,12 @@
+"""
+Core view for the app.
+"""
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def health_check(request):
+    """Returns successful response."""
+    return Response({'health': True})
